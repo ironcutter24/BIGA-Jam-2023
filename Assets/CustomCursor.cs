89bd15d55b0d9d4da77636bd1b8f9fb07e28fa81
Damiano Tagliaferri <ironcutter24@gmail.com>
2023-09-03 23:03:06 +0200

    Cursor

---
+++ Assets/CustomCursor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class CustomCursor : MonoBehaviour
+{
+    [SerializeField]
+    Image image;
+
+    [SerializeField]
+    Sprite cursorUp, cursorDown;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        image.sprite = Mouse.current.leftButton.isPressed ? cursorDown : cursorUp;
+        transform.position = Mouse.current.position.value;
+    }
+}
