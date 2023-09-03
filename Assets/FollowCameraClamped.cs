87822a4e5ced2b650521f02d0bd0f0241421b7fb
Damiano Tagliaferri <ironcutter24@gmail.com>
2023-09-03 18:08:48 +0200

    Infinite scroll water

---
+++ Assets/FollowCameraClamped.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FollowCameraClamped : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 step = new Vector2(10f, 10f);
+
+    [SerializeField]
+    Transform target;
+
+    void FixedUpdate()
+    {
+        var delta = target.position - transform.position;
+        delta.x = Snap(delta.x, step.x);
+        delta.y = Snap(delta.y, step.y);
+        delta.z = 0f;
+
+        transform.position += delta;
+    }
+
+    float Snap(float val, float step)
+    {
+        return Mathf.FloorToInt(val / step) * step;
+    }
+}
